1a610c8563815eb0d753b4fff7716258ddb6866e
Yong Git <1013588891@qq.com>
2022-08-05 15:27:49 +0800

    feat: v2.4.9

--- packages/hooks/src/data-design/transforms/reverse.ts
+++
@@ -1,8 +0,0 @@
-import { Transforms } from './index'
-
-Transforms.registerTransform('reverse', (dataSource: any[]) => {
-	if (dataSource != undefined && dataSource !== null) {
-		return dataSource.reverse()
-	}
-	return dataSource
-})
