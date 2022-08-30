6921e875c9520eb048737c148fd41be0464ff509
Yong Git <1013588891@qq.com>
2022-08-30 14:48:33 +0800

    delete: playground

--- playground/src/download/template/main.js
+++
@@ -1,4 +0,0 @@
-import App from "./App.vue";
-import { createApp } from "vue";
-
-createApp(App).use(VarletUI).mount("#app");
--- playground/src/utils/encode.ts
+++
@@ -1,9 +0,0 @@
-// prefer old unicode hacks for backward compatibility
-// https://base64.guru/developers/javascript/examples/unicode-strings
-export function utoa(data: string): string {
-  return btoa(unescape(encodeURIComponent(data)))
-}
-
-export function atou(base64: string): string {
-  return decodeURIComponent(escape(atob(base64)))
-}
