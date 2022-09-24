d827abaad511226d20d5ab81a632740d4a15ce11
Yong Git <1013588891@qq.com>
2022-09-24 10:47:56 +0800

    feat: useFetchs

---
+++ packages/hooks/src/useFetchs/index.ts
@@ -0,0 +1,81 @@
+import { Service, Options } from '@/useRequest/types'
+import { ref, watch } from 'vue'
+import useRequest from '../useRequest/index'
+
+const DEFAULT_KEY = 'VUE_HOOKS_PLUS_USE_REQUEST_DEFAULT_KEY'
+
+type FetchType<TData, TParams> = Record<
+  string | number,
+  {
+    data: TData | undefined
+    params: TParams
+    loading: boolean
+    key: string | number
+  }
+>
+
+type ParamsType<P> = P extends any[] ? any[] : any
+
+function useFetchs<TData, TParams>(
+  service: Service<TData, ParamsType<TParams>>,
+  options: Options<TData, ParamsType<TParams>, any> & {
+    manual: true
+  },
+  self: {
+    fetchKey?: (...args: ParamsType<TParams>) => string
+  },
+) {
+  const fetchKeyPersist = ref(self?.fetchKey)
+
+  const fetchs = ref<FetchType<TData, TParams>>({})
+  const newFetchs = ref<FetchType<TData, TParams>>({})
+
+  const getFetchs = (fetchs_: Record<string | number, any>) => {
+    newFetchs.value = fetchs_
+  }
+
+  const fetchRun = (...args: TParams extends any[] ? any[] : any) => {
+    const newstFetchKey = ref()
+    const cacheKey = fetchKeyPersist.value?.(...args) ?? DEFAULT_KEY
+    newstFetchKey.value = cacheKey
+
+    const { data, run, params, loading } = useRequest(service, {
+      ...options,
+      cacheKey,
+      manual: true,
+    })
+
+    // @ts-ignore
+    fetchs.value[cacheKey as string] = { key: cacheKey, data, params, loading }
+
+    getFetchs(fetchs.value)
+
+    run(...args)
+
+    watch([data, params, loading, newstFetchKey], curr => {
+      const [
+        newData = undefined,
+        newParams = undefined,
+        newLoading = false,
+        key = DEFAULT_KEY,
+      ] = curr
+      fetchs.value[key as string] = {
+        key: key as string | number,
+        // @ts-ignore
+        data: newData,
+        // @ts-ignore
+        params: newParams,
+        // @ts-ignore
+        loading: newLoading,
+      }
+      getFetchs(fetchs.value)
+    })
+  }
+
+  return {
+    fetchs: newFetchs,
+    fetchRun,
+  }
+}
+
+export default useFetchs
--- packages/hooks/src/useRequest/docs/basic/demo/demo.vue
+++ packages/hooks/src/useRequest/docs/basic/demo/demo.vue
@@ -1,19 +1,27 @@
 <template>
-  <div>读取用户名称：{{ data }}</div>
+  <div>读取名称：{{ data }}</div>
+  <div>读取名称1：{{ data1 }}</div>
+  <br>
+  <div>参数固定的情况下，是无法获取params的值{{ JSON.stringify(params) }}</div>
+  <div>手动请求的时候{{ JSON.stringify(params1) }}</div>
 </template>
 
 <script lang="ts" setup>
   import { useRequest } from 'vue-hooks-plus'
 
-  function getUsername(): Promise<string> {
+  function getUsername(params: { desc: string }): Promise<string> {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        resolve('vue-hooks-plus')
+        resolve(`vue-hooks-plus ${params.desc}`)
       }, 1000)
     })
   }
 
-  const { data } = useRequest(() => getUsername())
+  const { data, params } = useRequest(() => getUsername({ desc: '牛' }))
+
+  const { data: data1, params: params1, run } = useRequest(getUsername, { manual: true })
+
+  run({ desc: '小牛' })
 </script>
 
 <style scoped lang="less"></style>
