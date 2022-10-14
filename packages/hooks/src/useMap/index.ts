68f94f39594f6351cb9feed4808eb3287314db57
Yong Git <1013588891@qq.com>
2022-10-14 14:44:13 +0800

    fix: use-map-reset

--- packages/hooks/src/useMap/index.ts
+++ packages/hooks/src/useMap/index.ts
@@ -3,40 +3,41 @@ import { ref, Ref, markRaw } from 'vue'
 type MapValue<K, T> = Iterable<readonly [K, T]>
 
 type Actions<K, T> = {
-	set: (key: K, value: T) => void
-	get: (key: K) => T | undefined
-	remove: (key: K) => void
-	has: (key: K) => boolean
-	clear: () => void
-	setAll: (newMap: MapValue<K, T>) => void
-	reset: () => void
+  set: (key: K, value: T) => void
+  get: (key: K) => T | undefined
+  remove: (key: K) => void
+  has: (key: K) => boolean
+  clear: () => void
+  setAll: (newMap: MapValue<K, T>) => void
+  reset: () => void
 }
 
-function useMap<K, T>(
-	initialValue?: MapValue<K, T>
-): [Ref<Map<K, T>>, Actions<K, T>] {
-	const initialMap = initialValue ? new Map(initialValue) : new Map()
-	const state = ref(initialMap) as Ref<Map<K, T>>
+function useMap<K, T>(initialValue?: MapValue<K, T>): [Ref<Map<K, T>>, Actions<K, T>] {
+  const getInitValue = () => {
+    return initialValue ? new Map(initialValue) : new Map()
+  }
 
-	const actions: Actions<K, T> = {
-		set: (key, value) => {
-			state.value.set(key, value)
-		},
-		get: (key) => {
-			return state.value.get(key)
-		},
-		remove: (key) => {
-			state.value.delete(key)
-		},
-		has: (key) => state.value.has(key),
-		clear: () => state.value.clear(),
-		setAll: (newMap) => {
-			state.value = new Map(newMap)
-		},
-		reset: () => (state.value = initialMap),
-	}
+  const state = ref(getInitValue()) as Ref<Map<K, T>>
 
-	return [state, markRaw(actions)]
+  const actions: Actions<K, T> = {
+    set: (key, value) => {
+      state.value.set(key, value)
+    },
+    get: key => {
+      return state.value.get(key)
+    },
+    remove: key => {
+      state.value.delete(key)
+    },
+    has: key => state.value.has(key),
+    clear: () => state.value.clear(),
+    setAll: newMap => {
+      state.value = new Map(newMap)
+    },
+    reset: () => (state.value = getInitValue()),
+  }
+
+  return [state, markRaw(actions)]
 }
 
 export default useMap
