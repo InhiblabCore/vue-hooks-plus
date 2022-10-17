052a4e6268e8fcbf4225b9717ea96ed1eb4f2242
Yong Git <1013588891@qq.com>
2022-10-17 09:51:09 +0800

    test: use-favicon

--- packages/hooks/src/useFavicon/__tests__/index.spec.ts
+++ packages/hooks/src/useFavicon/__tests__/index.spec.ts
@@ -1,5 +1,7 @@
+import { mount } from '@vue/test-utils'
 import { ref } from 'vue'
 import useFavicon from '..'
+import Demo from '../demo/demo.vue'
 
 const DEFAULT_FAVICON_URL = 'https://nelsonyong.github.io/hooks-doc/favicon.ico'
 const GOOGLE_FAVICON_URL = 'https://www.google.com/favicon.ico'
@@ -11,9 +13,24 @@ describe('useFavicon', () => {
     expect(url.value).toBe(DEFAULT_FAVICON_URL)
   })
 
-  it('should work', () => {
+  it('should work', async () => {
     url.value = GOOGLE_FAVICON_URL
     expect(url.value).toBe(GOOGLE_FAVICON_URL)
+
+    const wrapper = mount(Demo)
+
+    const currentFaviconURL = wrapper.find('span')
+
+    const toggleToGoogleBtn = wrapper.findAll('vhp-button').at(0)
+    const toggleToAHooksBtn = wrapper.findAll('vhp-button').at(1)
+
+    expect(currentFaviconURL.text()).toBe(DEFAULT_FAVICON_URL)
+
+    await toggleToGoogleBtn?.trigger('click')
+    expect(currentFaviconURL.text()).toBe(GOOGLE_FAVICON_URL)
+
+    await toggleToAHooksBtn?.trigger('click')
+    expect(currentFaviconURL.text()).toBe(DEFAULT_FAVICON_URL)
   })
 
   it('support undefined', () => {
