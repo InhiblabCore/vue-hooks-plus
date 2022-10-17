05f30c4ae18c8a45530cfaceae05c8feda1d2d9b
Yong Git <1013588891@qq.com>
2022-10-17 11:43:36 +0800

    test: use-throttle

--- packages/hooks/src/useThrottle/demo/demo.vue
+++ packages/hooks/src/useThrottle/demo/demo.vue
@@ -1,16 +1,16 @@
 <template>
-	<div>
-		<input type="text" placeholder="输入" v-model="valueRef" />
-		<p>500ms后变化：{{ throttledValue }}</p>
-	</div>
+  <div>
+    <input type="text" placeholder="输入" v-model="valueRef">
+    <p>500ms后变化：{{ throttledValue }}</p>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+  import { ref } from 'vue'
 
-import { useThrottle } from 'vue-hooks-plus'
-const valueRef = ref('')
+  import { useThrottle } from 'vue-hooks-plus'
+  const valueRef = ref('')
 
-const throttledValue = useThrottle(valueRef, { wait: 500 })
+  const throttledValue = useThrottle(valueRef, { wait: 500 })
 </script>
 <style scoped lang="less"></style>
