994d86e4b79091255cd62b1e70d196d277bddc33
Yong Git <1013588891@qq.com>
2022-10-17 11:43:11 +0800

    fix: use-throttle

--- packages/hooks/src/useThrottle/index.ts
+++ packages/hooks/src/useThrottle/index.ts
@@ -2,23 +2,31 @@ import { Ref, ref, watch } from 'vue'
 import useThrottleFn from '../useThrottleFn'
 
 export interface ThrottleOptions {
-	wait?: number
-	leading?: boolean
-	trailing?: boolean
+  wait?: number
+  leading?: boolean
+  trailing?: boolean
 }
 
 function useThrottle<T>(value: Ref<T>, options?: ThrottleOptions) {
-	const throttled = ref()
+  const throttled = ref()
 
-	const { run } = useThrottleFn(() => {
-		throttled.value = value.value as any
-	}, options)
+  throttled.value = value.value
 
-	watch(value, () => {
-		run.value()
-	})
+  const { run } = useThrottleFn(() => {
+    throttled.value = value.value
+  }, options)
 
-	return throttled
+  watch(
+    value,
+    () => {
+      run.value()
+    },
+    {
+      immediate: true,
+    },
+  )
+
+  return throttled
 }
 
 export default useThrottle
