2c2d32c2166fa4ae7801be0ad7857c96fd6bdc35
Yong Git <1013588891@qq.com>
2022-10-20 13:17:07 +0800

    test: use-request/loadingDelay

---
+++ packages/hooks/src/useRequest/__tests__/delay.spec.ts
@@ -0,0 +1,18 @@
+import { sleep } from '@/utils/sleep'
+import { mount } from '@vue/test-utils'
+
+import Demo from '../docs/loadingDelay/demo/demo.vue'
+
+describe('useRequest/LoadingDelay', () => {
+  it(' should data work ', async () => {
+    const wrapper = mount(Demo)
+    const text = wrapper.findAll('div').at(1)
+
+    expect(text?.text()).toBe('读取用户名称B：')
+    await sleep(301)
+    expect(text?.text()).toBe('读取用户名称B：loading...')
+
+    await sleep(1000)
+    expect(text?.text()).toBe('读取用户名称B：vue-hooks-plus useRequest A')
+  })
+})
