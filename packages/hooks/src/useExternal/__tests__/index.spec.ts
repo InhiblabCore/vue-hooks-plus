94eb622d8b956bce7c32cb4128cb43eaeee45e0d
Yong Git <1013588891@qq.com>
2022-10-20 11:14:30 +0800

    test: use-external

---
+++ packages/hooks/src/useExternal/__tests__/index.spec.ts
@@ -0,0 +1,26 @@
+import { sleep } from '@/utils/sleep'
+import { mount } from '@vue/test-utils'
+import useExternal from '..'
+import Demo from '../demo/demo.vue'
+
+describe('useExternal', () => {
+  const wrapper = mount(Demo)
+
+  it('should load axios js and fetch API', () => {
+    const demoText = wrapper.find('.city')
+    expect(demoText.text()).toBe('Result: 星期一天气')
+
+    const status = useExternal('https://cdn.bootcdn.net/ajax/libs/axios/0.26.1/axios.js', {
+      js: {
+        async: true,
+      },
+    })
+
+    expect(status.value).toBeTruthy()
+  })
+
+  it('should load css', () => {
+    const status = useExternal('https://ahooks.js.org/useExternal/bootstrap-badge.css')
+    expect(status.value).toBeTruthy()
+  })
+})
--- packages/hooks/src/useExternal/demo/demo.vue
+++ packages/hooks/src/useExternal/demo/demo.vue
@@ -1,39 +1,47 @@
 <template>
-	<div style="overflow: scroll;height: 600px;">
-		<div>Status: {{ status }}</div>
-		<div>Result: {{ `${data?.data?.city ?? ''}星期一天气` }}</div>
-		<div>{{ `${JSON.stringify(data?.data?.data?.[0])}` }}</div>
-	</div>
+  <div style="overflow: scroll;min-height: 200px;">
+    <div>Status: {{ status }}</div>
+    <div class="city">Result: {{ `${city ?? ''}星期一天气` }}</div>
+    <div>
+      <div v-for="(item, index) in data" :key="index">
+        <div>{{ item.date }}: {{ item.wea }} （{{ item.tem }}）</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { watchEffect, ref } from 'vue'
+  import { watchEffect, ref } from 'vue'
 
-import { useExternal } from 'vue-hooks-plus'
+  import { useExternal } from 'vue-hooks-plus'
 
-const status = useExternal(
-	'https://cdn.bootcdn.net/ajax/libs/axios/0.26.1/axios.js',
-	{
-		js: {
-			async: true,
-		},
-	}
-)
+  const status = useExternal('https://cdn.bootcdn.net/ajax/libs/axios/0.26.1/axios.js', {
+    js: {
+      async: true,
+    },
+  })
 
-const data = ref()
+  const data = ref()
+  const city = ref()
 
-watchEffect(() => {
-	if (status.value === 'ready') {
-		// @ts-ignore
-		axios
-			.get(
-				'https://yiketianqi.com/api?unescape=1&version=v1&appid=85841439&appsecret=EKCDLT4I&city=广州'
-			)
-			.then((res: any) => {
-				data.value = res
-			})
-	}
-})
+  watchEffect(() => {
+    if (status.value === 'ready') {
+      // @ts-ignore
+      axios
+        .get(
+          'https://yiketianqi.com/api?unescape=1&version=v1&appid=85841439&appsecret=EKCDLT4I&city=广州',
+        )
+        .then((res: any) => {
+          city.value = res?.data?.city ?? ''
+          data.value =
+            (res?.data?.data as any[])?.map(item => ({
+              date: item.date,
+              wea: item.wea,
+              tem: item.tem,
+            })) ?? []
+        })
+    }
+  })
 </script>
 
 <style scoped lang="less"></style>
