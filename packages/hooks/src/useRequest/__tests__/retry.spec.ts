a647358c3e8922cf66d433b9bb8ac8580e5ba38c
Yong Git <1013588891@qq.com>
2022-10-20 14:17:23 +0800

    test: useRequest/retry

---
+++ packages/hooks/src/useRequest/__tests__/retry.spec.ts
@@ -0,0 +1,20 @@
+import { sleep } from '@/utils/sleep'
+import { mount } from '@vue/test-utils'
+import Demo from '../docs/retry/demo/demo.vue'
+
+describe('useRequest/Retry', () => {
+  const wrapper = mount(Demo)
+
+  const retryText = wrapper.find('span')
+
+  it('should init 0', () => {
+    expect(retryText.text()).toBe('0')
+  })
+
+  it('should auto work', async () => {
+    await sleep(1000)
+    expect(retryText.text()).toBe('1')
+    await sleep(3000)
+    expect(retryText.text()).toBe('2')
+  })
+})
--- packages/hooks/src/useRequest/docs/retry/demo/demo.vue
+++ packages/hooks/src/useRequest/docs/retry/demo/demo.vue
@@ -1,28 +1,30 @@
 <template>
-	<div style="margin-top: 16px;">请求错误次数：{{ count }}</div>
+  <div style="margin-top: 16px;"
+  >请求错误次数： <span>{{ count }}</span></div
+  >
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+  import { ref } from 'vue'
 
-import { useRequest } from 'vue-hooks-plus'
+  import { useRequest } from 'vue-hooks-plus'
 
-function getUsername(): Promise<string> {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			reject(`error`)
-		}, 1000)
-	})
-}
+  function getUsername(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject(`error`)
+      }, 1000)
+    })
+  }
 
-const count = ref(0)
+  const count = ref(0)
 
-useRequest(() => getUsername(), {
-	retryCount: 3,
-	onError: () => {
-		count.value += 1
-	},
-})
+  useRequest(() => getUsername(), {
+    retryCount: 3,
+    onError: () => {
+      count.value += 1
+    },
+  })
 </script>
 
 <style scoped lang="less"></style>
