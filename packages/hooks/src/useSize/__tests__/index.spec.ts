890d505627765aaa6953b7374848a330944bf63d
YongGit <1013588891@qq.com>
2022-12-12 16:24:01 +0800

    test: use-size

--- packages/hooks/src/useSize/__tests__/index.spec.ts
+++ packages/hooks/src/useSize/__tests__/index.spec.ts
@@ -2,15 +2,15 @@ import renderHook from 'test-utils/renderHook'
 import useSize from '../index'
 
 let callback: (arg0: { target: { clientWidth: number; clientHeight: number } }[]) => void
-vitest.mock('resize-observer-polyfill', () => {
-  return vitest.fn().mockImplementation(cb => {
-    callback = cb
-    return {
-      observe: () => {},
-      disconnect: () => {},
-    }
-  })
-})
+// vitest.mock('resize-observer-polyfill', () => {
+//   return vitest.fn().mockImplementation(cb => {
+//     callback = cb
+//     return {
+//       observe: () => {},
+//       disconnect: () => {},
+//     }
+//   })
+// })
 
 describe('useSize', () => {
   it('should not work when target is null', () => {
