becfb74e4d80864d520b7f94022021ddecbd0f03
YongGit <1013588891@qq.com>
2022-12-12 14:43:33 +0800

    test: use-net-work

--- packages/hooks/src/useNetwork/index.ts
+++ packages/hooks/src/useNetwork/index.ts
@@ -1,88 +1,86 @@
+/* eslint-disable no-unused-vars */
 import { watchEffect, ref } from 'vue'
 
 export interface NetworkState {
-	since?: Date
-	online?: boolean
-	rtt?: number
-	type?: string
-	downlink?: number
-	saveData?: boolean
-	downlinkMax?: number
-	effectiveType?: string
+  since?: Date
+  online?: boolean
+  rtt?: number
+  type?: string
+  downlink?: number
+  saveData?: boolean
+  downlinkMax?: number
+  effectiveType?: string
 }
 
 enum NetworkEventType {
-	ONLINE = 'online',
-	OFFLINE = 'offline',
-	CHANGE = 'change',
+  ONLINE = 'online',
+  OFFLINE = 'offline',
+  CHANGE = 'change',
 }
 
 function getConnection() {
-	const nav = navigator as any
-	if (typeof nav !== 'object') return null
-	return nav.connection || nav.mozConnection || nav.webkitConnection
+  const nav = navigator as any
+  if (typeof nav !== 'object') return null
+  return nav.connection || nav.mozConnection || nav.webkitConnection
 }
 
 function getConnectionProperty(): NetworkState {
-	const c = getConnection()
-	if (!c) return {}
-	return {
-		rtt: c.rtt,
-		type: c.type,
-		saveData: c.saveData,
-		downlink: c.downlink,
-		downlinkMax: c.downlinkMax,
-		effectiveType: c.effectiveType,
-	}
+  const c = getConnection()
+  if (!c) return {}
+  return {
+    rtt: c.rtt,
+    type: c.type,
+    saveData: c.saveData,
+    downlink: c.downlink,
+    downlinkMax: c.downlinkMax,
+    effectiveType: c.effectiveType,
+  }
 }
 
 function useNetwork(): NetworkState {
-	const state = ref({
-		since: undefined,
-		online: navigator?.onLine,
-		...getConnectionProperty(),
-	})
-	watchEffect((onInvalidate) => {
-		const onOnline = () => {
-			state.value = {
-				...state.value,
-				online: true,
-				since: new Date(),
-			}
-		}
+  const state = ref({
+    since: undefined,
+    online: navigator?.onLine,
+    ...getConnectionProperty(),
+  })
+  watchEffect(onInvalidate => {
+    const onOnline = () => {
+      state.value = {
+        ...state.value,
+        online: true,
+        since: new Date(),
+      }
+    }
 
-		const onOffline = () => {
-			state.value = {
-				...state.value,
-				online: false,
-				since: new Date(),
-			}
-		}
+    const onOffline = () => {
+      state.value = {
+        ...state.value,
+        online: false,
+        since: new Date(),
+      }
+    }
 
-		const onConnectionChange = () => {
-			state.value = {
-				...state.value,
-				...getConnectionProperty(),
-			}
-		}
+    const onConnectionChange = () => {
+      state.value = {
+        ...state.value,
+        ...getConnectionProperty(),
+      }
+    }
 
-		window.addEventListener(NetworkEventType.ONLINE, onOnline)
-		window.addEventListener(NetworkEventType.OFFLINE, onOffline)
+    window.addEventListener(NetworkEventType.ONLINE, onOnline)
+    window.addEventListener(NetworkEventType.OFFLINE, onOffline)
 
-		const connection = getConnection()
-		connection?.addEventListener(NetworkEventType.CHANGE, onConnectionChange)
+    const connection = getConnection()
+    connection?.addEventListener(NetworkEventType.CHANGE, onConnectionChange)
 
-		onInvalidate(() => {
-			window.removeEventListener(NetworkEventType.ONLINE, onOnline)
-			window.removeEventListener(NetworkEventType.OFFLINE, onOffline)
-			connection?.removeEventListener(
-				NetworkEventType.CHANGE,
-				onConnectionChange
-			)
-		})
-	})
+    onInvalidate(() => {
+      window.removeEventListener(NetworkEventType.ONLINE, onOnline)
+      window.removeEventListener(NetworkEventType.OFFLINE, onOffline)
+      connection?.removeEventListener(NetworkEventType.CHANGE, onConnectionChange)
+    })
+  })
 
-	return state.value
+  return state.value
 }
 
 export default useNetwork
