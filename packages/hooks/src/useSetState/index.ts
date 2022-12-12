5b37a9a186dd24841de2d636c6235bf17da8663c
YongGit <1013588891@qq.com>
2022-12-12 14:43:59 +0800

    test: use-set-state

--- packages/hooks/src/useSetState/index.ts
+++ packages/hooks/src/useSetState/index.ts
@@ -1,5 +1,5 @@
 import { ref, Ref, unref, UnwrapRef, readonly, DeepReadonly, UnwrapNestedRefs } from 'vue'
-import { merge } from 'lodash'
+import merge from 'lodash/merge'
 
 type StateType<S> = S | (() => S) | Ref<S> | (() => Ref<S>)
 
