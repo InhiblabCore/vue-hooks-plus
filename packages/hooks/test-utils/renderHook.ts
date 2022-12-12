37bb7259b8165d46af58ab5325e21d2d77934a7e
YongGit <1013588891@qq.com>
2022-12-12 14:44:33 +0800

    test: render-hook support act

--- packages/hooks/test-utils/renderHook.ts
+++ packages/hooks/test-utils/renderHook.ts
@@ -1,15 +1,27 @@
 import { App, createApp, defineComponent } from 'vue'
 
-export default function renderHook<R = any>(renderFC: () => R): [R, App<Element>] {
-let result: any
+export default function renderHook<R = any>(
+  renderFC: () => R,
+): [
+  R,
+  App<Element>,
+  {
+    act?: (fn: () => void) => void
+  },
+] {
+  let result: any
+  let act
   const app = createApp(
     defineComponent({
       setup() {
         result = renderFC()
+        act = (fn: () => void) => {
+          fn()
+        }
         return () => {}
       },
     }),
   )
   app.mount(document.createElement('div'))
- return [result, app]
+  return [result, app, { act }]
 }
