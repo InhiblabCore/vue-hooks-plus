067a93f566b8260c21d85a6187a522965ff8f1bd
YongGit <1013588891@qq.com>
2023-03-20 15:37:04 +0800

    feat: add use-request resolver

---
+++ packages/resolvers/src/unplugin-auto-import-resolver/useRequestResolver.ts
@@ -0,0 +1,49 @@
+import { Resolver } from 'unplugin-auto-import/types'
+let hooks: string[] | undefined
+
+export type VueHooksPlusResolverOptions = {
+  /**
+   * prefix for name of components
+   *
+   * @default ''
+   */
+  prefix?: string
+}
+
+function queryMetaData() {
+  try {
+    hooks = ['useRequest']
+  } catch (e) {
+    console.error(e)
+    throw new Error(
+      '[@vue-hooks-plus/use-request:plugins] failed to load @vue-hooks-plus/use-worker, have you installed it?',
+    )
+  }
+}
+
+function resolveHooks(name: string, options: VueHooksPlusResolverOptions) {
+  if (!hooks) return
+
+  const { prefix } = options
+  if (prefix) {
+    if (!name.startsWith(prefix)) return
+    name = name.substring(prefix.length)
+  }
+  if (!hooks.includes(name)) return
+
+  return {
+    name,
+    from: '@vue-hooks-plus/use-request',
+  }
+}
+
+export default function VueHooksPlusUseRequestResolver(
+  options: VueHooksPlusResolverOptions = {},
+): Resolver {
+  return name => {
+    if (!hooks) {
+      queryMetaData()
+    }
+    return resolveHooks(name, options)
+  }
+}
