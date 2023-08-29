fa2606e69bd15ed3fa5ba48e9089c57b006b9a21
YongGit <1013588891@qq.com>
2023-08-29 21:48:00 +0800

    feat: add sync script for use-request

---
+++ packages/hooks/src/useRequest/utils/index.ts
@@ -0,0 +1,8 @@
+export const isObject = (value: unknown): value is Record<any, any> =>
+  value !== null && typeof value === 'object'
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const isFunction = (value: unknown): value is Function => typeof value === 'function'
+export const isString = (value: unknown): value is string => typeof value === 'string'
+export const isBoolean = (value: unknown): value is boolean => typeof value === 'boolean'
+export const isNumber = (value: unknown): value is number => typeof value === 'number'
+export const isUndef = (value: unknown): value is undefined => typeof value === 'undefined'
