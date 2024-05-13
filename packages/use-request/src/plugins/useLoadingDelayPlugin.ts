3676e775bc45d625ac2f5bab5a21f47bfab33e71
YongGit <1013588891@qq.com>
2024-05-13 22:18:12 +0800

    feat: optimized

--- packages/use-request/src/plugins/useLoadingDelayPlugin.ts
+++ packages/use-request/src/plugins/useLoadingDelayPlugin.ts
@@ -1,44 +1,47 @@
-import { unref, ref } from "vue";
-import type { UseRequestPlugin, Timeout } from "../types";
+import { ref, unref } from 'vue'
+import { Timeout, UseRequestPlugin } from '../types'
 
-const useLoadingDelayPlugin: UseRequestPlugin<unknown, unknown[]> = (
-  fetchInstance,
-  { loadingDelay }
-) => {
-  const timerRef = ref<Timeout>();
-  if (!unref(loadingDelay)) {
-    return {};
+const useLoadingDelayPlugin: UseRequestPlugin<unknown, unknown[]> = (inst, { loadingDelay }) => {
+  const delayRef = ref<Timeout>()
+
+  const clear = () => {
+    if (delayRef.value) {
+      clearTimeout(unref(delayRef.value))
+
+      delayRef.value = undefined
+    }
   }
 
-  const cancelTimeout = () => {
-    if (timerRef.value) {
-      clearTimeout(timerRef.value);
-    }
-  };
-
-
-
   return {
-    name: "loadingDelayPlugin",
-    onBefore: () => {
-      cancelTimeout();
-      timerRef.value = setTimeout(() => {
-        fetchInstance.setState({
-          loading: true,
-        });
-      }, (unref(loadingDelay)) as number);
-
-      return {
-        loading: false,
-      };
-    },
+    name: 'loadingDelayPlugin',
     onFinally: () => {
-      cancelTimeout();
-    },
-    onCancel: () => {
-      cancelTimeout();
-    },
-  };
-};
+      clear()
 
-export default useLoadingDelayPlugin;
+      const delay = unref(loadingDelay)
+
+      /**
+       *
+       * if loadingDelay is set, the loading state will be delayed,
+       * until the delay time is reached.
+       *
+       * if delay is set to 0, the loading state will not be delayed. because 0 is mean nothing.
+       */
+      if (delay) {
+        inst.setState({
+          loading: true,
+        })
+
+        delayRef.value = setTimeout(() => {
+          inst.setState({
+            loading: false,
+          })
+        }, delay)
+      }
+    },
+    onError: () => {
+      clear()
+    },
+  }
+}
+
+export default useLoadingDelayPlugin
