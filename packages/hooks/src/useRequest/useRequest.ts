3b72edd7cfe104212feac173740a483eb435ddd5
YongGit <1013588891@qq.com>
2024-05-15 08:48:00 +0800

    fix: use-request typing (#201)

--- packages/hooks/src/useRequest/useRequest.ts
+++ packages/hooks/src/useRequest/useRequest.ts
@@ -20,7 +20,7 @@ export function useRequest<
   TParams extends unknown[] = unknown[],
   PluginsOptions extends UseRequestPlugin<TData, TParams>[] = UseRequestPlugin<TData, TParams>[],
   SR = any,
->(service: UseRequestService<SR, TParams>, options?: UseRequestOptionsWithFormatResult<TData, TParams, PluginsOptions extends (infer P)[]
+>(service: UseRequestService<SR, TParams>, options: UseRequestOptionsWithFormatResult<TData, TParams, PluginsOptions extends (infer P)[]
   ? P extends UseRequestPlugin<TData, TParams, infer R>
   ? R
   : never
