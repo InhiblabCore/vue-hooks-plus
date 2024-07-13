5302c1ada412b0bdb34eb3e80d7e5c387733ff84
YongGit <1013588891@qq.com>
2024-07-13 11:43:43 +0800

    fix: use-interval no clear func (#222)
    * fix: use-interval no clear func
    
    * chore: github ci
    
    * chore: script command

--- packages/hooks/src/useInterval/index.ts
+++ packages/hooks/src/useInterval/index.ts
@@ -16,28 +16,59 @@ function useInterval(
      */
     immediate?: boolean
   },
-): void {
+): {
+  /**
+   * Clear the interval timer.
+   */
+  clear: VoidFunction
+  /**
+   * Restart the interval timer.
+   */
+  restart: VoidFunction
+} {
   const immediate = options?.immediate
 
   const fnRef = ref(fn)
 
-  watchEffect(onInvalidate => {
+  const timerRef = ref<ReturnType<typeof setInterval> | null>(null)
+
+  const setupInterval = () => {
     if (isRef(delay)) {
       if (typeof delay.value !== 'number' || delay.value < 0) return
     } else {
       if (typeof delay !== 'number' || delay < 0) return
     }
+
     if (immediate) {
       fnRef.value()
     }
+
     const _deply = unref(delay)
-    const timer = setInterval(() => {
+    timerRef.value = setInterval(() => {
       fnRef.value()
     }, _deply)
-    onInvalidate(() => {
-      clearInterval(timer)
-    })
+  }
+
+  const clear = () => {
+    if (timerRef.value) {
+      clearInterval(timerRef.value)
+    }
+  }
+
+  const restart = () => {
+    clear()
+    setupInterval()
+  }
+
+  watchEffect(onInvalidate => {
+    setupInterval()
+    onInvalidate(clear)
   })
+
+  return {
+    clear,
+    restart,
+  }
 }
 
 export default useInterval
