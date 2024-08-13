f9860e08acfaeaa43fea3efde13f39feaf733b28
NelsonYong <1013588891@qq.com>
2024-08-13 23:57:06 +0800

    feat: will use custom tool lodash

---
+++ packages/tools/lodash/.internal/freeGlobal.ts
@@ -0,0 +1,5 @@
+/** Detect free variable `global` from Node.js. */
+const freeGlobal =
+  typeof global === 'object' && global !== null && global.Object === Object && global
+
+export default freeGlobal
