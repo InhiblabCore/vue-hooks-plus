9c5f28daa4593535f014c8d35f569667137e6a0d
YongGit <1013588891@qq.com>
2025-03-02 22:23:01 +0800

    feat: sync

--- packages/use-request/src/plugins/useDebouncePlugin.ts
+++ packages/use-request/src/plugins/useDebouncePlugin.ts
@@ -1,66 +1,60 @@
-import { ref, computed, watchEffect, unref } from "vue";
-import type { DebouncedFunc, DebounceSettings } from "lodash";
-import debounce from "lodash/debounce";
-import type { UseRequestPlugin } from "../types";
+import { ref, computed, watchEffect, unref,  } from "vue"
+import debounce from "lodash/debounce"
+import type { UseRequestPlugin } from "../types"
+import type { DebouncedFunc, DebounceSettings } from 'lodash'
 
 const useDebouncePlugin: UseRequestPlugin<unknown, unknown[]> = (
   fetchInstance,
-  { debounceWait, debounceLeading, debounceTrailing, debounceMaxWait }
+  { debounceWait, debounceLeading, debounceTrailing, debounceMaxWait },
 ) => {
-  const debouncedRef = ref<DebouncedFunc<any>>();
-  const options = computed(() => {
-    const ret: DebounceSettings = {};
-    const debounceLeading_ = unref(debounceLeading)
-    const debounceTrailing_ = unref(debounceTrailing)
-    const debounceMaxWait_ = unref(debounceMaxWait)
-    if (debounceLeading_ !== undefined) {
-      ret.leading = debounceLeading_;
-    }
-    if (debounceTrailing_ !== undefined) {
-      ret.trailing = debounceTrailing_
-    }
-    if (debounceMaxWait_ !== undefined) {
-      ret.maxWait = debounceMaxWait_;
-    }
-    return ret;
-  });
+  const debouncedRef = ref<DebouncedFunc<any>>()
 
-  watchEffect((onInvalidate) => {
+  const options = computed(() => {
+    const ops: DebounceSettings = {
+      leading: unref(debounceLeading),
+      trailing: unref(debounceTrailing),
+      maxWait: unref(debounceMaxWait),
+    }
+
+    return ops
+  })
+
+  watchEffect(onInvalidate => {
     if (unref(debounceWait)) {
-      const _originRunAsync = fetchInstance.runAsync.bind(fetchInstance);
+      const _originRunAsync = fetchInstance.runAsync.bind(fetchInstance)
+
       debouncedRef.value = debounce(
-        (callback: () => void) => {
-          callback();
-        },
-        unref(debounceWait),
-        options.value
-      );
-      fetchInstance.runAsync = (...args) => {
-        return new Promise((resolve, reject) => {
-          debouncedRef.value?.(() => {
+        (...args: any[]) => {
+          return new Promise((resolve, reject) => {
             _originRunAsync(...args)
               .then(resolve)
-              .catch(reject);
-          });
-        });
-      };
+              .catch(reject)
+          })
+        },
+        unref(debounceWait),
+        options.value,
+      )
+
       onInvalidate(() => {
-        debouncedRef.value?.cancel();
-        fetchInstance.runAsync = _originRunAsync;
-      });
+        debouncedRef.value?.cancel()
+      })
     }
-  });
+  })
 
   if (!unref(debounceWait)) {
-    return {};
+    return {
+      name: 'debouncePlugin',
+    }
   }
 
   return {
-    name: "debouncePlugin",
+    name: 'debouncePlugin',
     onCancel: () => {
-      debouncedRef.value?.cancel();
+      // 取消时同时执行 cancel 和 flush，确保不会有遗留的防抖函数
+      debouncedRef.value?.cancel()
+      debouncedRef.value?.flush()
     },
-  };
-};
+  }
+}
 
-export default useDebouncePlugin;
+export default useDebouncePlugin
