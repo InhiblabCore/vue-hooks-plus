4949df96b2c71001a899af6f4d284f2711194a73
YongGit <1013588891@qq.com>
2025-04-03 10:21:07 +0800

    docs: pwa

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -2,11 +2,26 @@ import { defineConfig } from 'vitepress'
 import { shared } from './shared'
 import { en } from './en'
 import { zh } from './zh'
+import { withPwa } from '@vite-pwa/vitepress'
+import { resolve } from 'path'
 
-export default defineConfig({
+export default withPwa(defineConfig({
   ...shared,
   locales: {
     root: { label: 'English', ...en },
     zh: { label: '简体中文', ...zh }
+  },
+  pwa: {
+    outDir: resolve(__dirname, '../', 'dist'),
+    registerType: 'autoUpdate',
+    srcDir: '.vitepress/',
+    strategies: 'generateSW', // <== if omitted, defaults to `generateSW`  
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}'], // 添加明确的glob模式
+      globDirectory: '../', // 指定glob目录
+    },
+    experimental: {
+      includeAllowlist: true
+    }
   }
-})
+}))
